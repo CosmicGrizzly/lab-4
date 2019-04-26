65ce30ab960c811add57956b47524d3c5b32d6a4
Jake Balch <InariOrchid@gmail.com>
2019-04-26 02:13:59 -0400

    lab 4

---
+++ Lab_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me your favorite number! ");
+            int userInput = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i <= userInput)
+            {
+
+                Console.WriteLine("{0,10}{1,10}{2,10}", "number ", "square ", "cube ");
+                Console.WriteLine("{0}", i++);
+                Console.WriteLine("{0,10}{1,10}{2,10}", i, i * i, i * i * i);
+                i++;
+            }
+            Console.WriteLine();
+        }
+    }
+}
